a97af477642ed2516a97bc9e2815db5db9a35160
hsc92180 <68774484+hsc92180@users.noreply.github.com>
2024-07-18 23:54:40 +0530

    add quiz-app project

---
+++ src/components/Quiz.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react";
+
+const questions = [
+  {
+    question: "What is the capital of America?",
+    options: ["New York City", "Miami", "Santa Fe", "Washington DC"],
+    answer: "Washington DC",
+  },
+  {
+    question: "What is the capital of France?",
+    options: ["Paris", "London", "Berlin", "Madrid"],
+    answer: "Paris",
+  },
+];
+
+export default function Quiz() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [shuffledQuestions, setShuffledQuestions] = useState([]);
+  const [wrongAnswers, setWrongAnswers] = useState([]);
+
+  useEffect(() => {
+    shuffleQuestions();
+  }, []);
+
+  const shuffleQuestions = () => {
+    const shuffledQues = questions.sort(() => Math.random() - 0.5);
+    setShuffledQuestions(shuffledQues);
+  };
+
+  const handleAnswerClick = (option) => {
+    setShowAnswer(true);
+    const currentQuestion = shuffledQuestions[currentQuestionIndex];
+    if (option === currentQuestion.answer) {
+      setScore(score + 1);
+    } else {
+      setWrongAnswers([...wrongAnswers, currentQuestion]);
+    }
+
+    setTimeout(() => {
+      setShowAnswer(false);
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }, 2000);
+  };
+
+  const resetQuiz = () => {
+    setScore(0);
+    setCurrentQuestionIndex(0);
+    setShowAnswer(false);
+    setWrongAnswers([]);
+    shuffleQuestions();
+  };
+
+  if (currentQuestionIndex >= shuffledQuestions.length) {
+    return (
+      <div className="quiz-container">
+        <h1>Quiz Completed</h1>
+        <p>Current Score: {score}</p>
+        <div className="question-card">
+          <h2>Wrong Answers</h2>
+          <div className="wrong-answers">
+          <ul>
+            {wrongAnswers.map((question, index) => (
+              <li key={index}>
+                {question.question} - Correct Answer: {question.answer}
+              </li>
+            ))}
+          </ul>
+          </div>
+        </div>
+        <button onClick={resetQuiz}>Reset Quiz</button>
+      </div>
+    );
+  }
+
+  const currentQuestion = shuffledQuestions[currentQuestionIndex];
+
+  return (
+    <div className="quiz-container">
+      <h1>Quiz</h1>
+      <p>Current Score: {score}</p>
+      <div className="question-card">
+        <p className="question-count">
+          Question {currentQuestionIndex + 1} out of {shuffledQuestions.length}
+        </p>
+        <h2 className="question-text">{currentQuestion.question}</h2>
+        <div className="options">
+          {currentQuestion.options.map((option, index) => (
+            <button
+              className="option-button"
+              key={index}
+              onClick={() => handleAnswerClick(option)}
+              disabled={showAnswer}
+            >
+              {option}
+            </button>
+          ))}
+        </div>
+        {showAnswer && (
+          <p className="correct-answer">
+            Correct Answer: {currentQuestion.answer}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
